da5b242acf13cb3456435255a26bf3d2cab0d801
hankesen <hankesen@yxqiche.com>
2022-06-16 16:06:29 +0800

    first commit

---
+++ ooooo/conv.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__() # just run the init of parent class (nn.Module)
+        self.conv1 = nn.Conv2d(1, 32, 5) # input is 1 image, 32 output channels, 5x5 kernel / window
+        self.conv2 = nn.Conv2d(32, 64, 5) # input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 kernel / window
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50,50).view(-1,1,50,50)
+        self._to_linear = None
+        self.convs(x)
+        
+    def convs(self, x):
+        # max pooling over 2x2
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before 
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x) # bc this is our output layer. No activation here.
+        return F.softmax(x, dim=1)
+
+net = Net()
+print(net)
